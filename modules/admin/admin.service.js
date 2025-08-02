2dd6dc5ce5ba7f12b98fd446fe73fa15944238e1
John Sokpo <john.sokpo@hotmail.com>
2025-08-03 00:26:17 +0100

    Update admin.service.js

--- modules/admin/admin.service.js
+++ modules/admin/admin.service.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AdminService = void 0;
 const client_1 = require("@prisma/client");
-const customError_1 = require("../../errors/customError");
-const constants_1 = require("../../constants");
+const customError_1 = require("../../errors/customError.js");
+const constants_1 = require("../../constants/index.ts");
 const prisma = new client_1.PrismaClient();
 exports.AdminService = {
     /**
